ae171db218752f697a8d18ffea51b323a9b5291c
Mujeeb Shaikh <mujeebs211@gmail.com>
2021-05-29 18:53:09 +0530

    routing with react router dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 //Import Pages
 import AboutUs from  './pages/AboutUs';
+import OurWork from  './pages/OurWork';
+import ContactUs from  './pages/ContactUs';
 //Global Style
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+//Router
+import {Switch, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs/>
+        </Route>
+        <Route path="/work">
+          <OurWork/>
+        </Route>
+        <Route path="/contact">
+          <ContactUs/>
+        </Route>
+      </Switch>
     </div>
   );
 }
